3b5057eed86b754edf9d4e195a8f17c455440e6c
Rohit Choukiker <rohitchoukiker2803@gmail.com>
2025-01-14 11:40:43 +0530

    auth

--- client/src/context/authcontext/index.jsx
+++ client/src/context/authcontext/index.jsx
@@ -7,7 +7,6 @@ const AuthContext = createContext();
 export function AuthProvider({ children }) {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
 
     useEffect(() => {
         checkUser();
@@ -32,49 +31,7 @@ export function AuthProvider({ children }) {
 
     }
 
-    // const register = async (userData) => {
-    //     setLoading(true);
-    //     setError(null);
-    //     try {
-    //         const response = await authAPI.register(userData);
-    //         setUser(response.user);
-    //         return response;
-    //     } catch (err) {
-    //         setError(err.response?.data?.message || 'Signup failed');
-    //         throw err;
-    //     } finally {
-    //         setLoading(false);
-    //     }
-    // }
 
-    // const login = async (credentials) => {
-    //     setLoading(true);
-    //     setError(null);
-    //     try {
-    //         const response = await authAPI.login(credentials);
-    //         setUser(response.user);
-    //         return response;
-    //     } catch (err) {
-    //         setError(err.response?.data?.message || 'Login failed');
-    //         throw err;
-    //     } finally {
-    //         setLoading(false);
-    //     }
-    // }
-
-    // const logout = async () => {
-    //     setLoading(true);
-    //     setError(null);
-    //     try {
-    //         await authAPI.logout();
-    //         setUser(null);
-    //     } catch (err) {
-    //         setError(err.response?.data?.message || 'Logout failed');
-    //         throw err;
-    //     } finally {
-    //         setLoading(false);
-    //     }
-    // }
 
     const value = {
         user
